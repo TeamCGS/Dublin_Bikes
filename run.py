529a7012eb68c7df7cb22fffded02de83bb4aaf4
EC2 Default User <ec2-user@ip-172-31-28-36.eu-west-1.compute.internal>
2018-04-19 21:48:35 +0000

    new cert and key pem files made to run on https and enable geolocation

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ app = create_app(config_name)
 
 if __name__ == '__main__':
     #app.run(host ='0.0.0.0',ssl_context='adhoc',debug='True')
-    app.run(host ='0.0.0.0',ssl_context=('cert.pem', 'key.pem'),debug='True')
+     app.run(host ='0.0.0.0',ssl_context=('cert.pem', 'key.pem'),debug='False')
+    # app.run(host='0.0.0.0')
